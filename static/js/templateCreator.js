80e86e4bac0c18cb2a16d7a395044a78f9e48f89
maliza sivongsa <malizasivongsa@malizas-MacBook-Air.local>
2021-11-09 15:56:35 -0600

    finally got the ability to add photocards to template creator

--- static/js/templateCreator.js
+++ static/js/templateCreator.js
@@ -1,26 +1,22 @@
-'use strict'
+'use strict';
 
-const jsonData = require('./photocards.json');
+// const jsonData = require('./photocards.json')
 
 // whatever pc the user chooses will show up in the creator-container
-const createDivsforPCs = jsonFile => {
-    for (const data of jsonFile) {
-        $('#creator-container').append(`<div class="pc_picked${data.pc_id}">${data.pc_img}</div>`);
-    }
-};
+// need to somehow implement an if statement for whenever the user picks a pc
+// so it's added to the creator-container (left side of column)
+const addPCToContainer = (data) => {
+    $('#creator-container').append(
+        `<img src="${data}" style="height:180px;width=auto" />`)
+}
 
-// takes whatever the pc the user picks from the list of pcs
-const userPicked = (evt) => {
-    evt.preventDefault;
-    $('.all_photocards').attr('{{ card.photocard_id }}')
-}  
-
-(function customTemplate() {
-    createDivsforPCs(jsonData);
-    userPicked();
-
-    $('#{{ card.photocard_id }}').on('click', evt => {
-        const clickedPC = evt.target;
-
-    })
-})();
+// takes whatever the pc the user picks from the list of pcs given (right side of column)
+$('input[type="checkbox"]').on('click', function(){
+    let selection = [];
+    $('input[type="checkbox"]:checked').each(function() {
+        selection.push($(this).val());
+    });
+    addPCToContainer($(this).val());
+    // if checked before then remove
+    // $('#creator-container').html(selection)
+});
