eb3491b6be0791a49886fab2a23554f32fba38dd
maliza sivongsa <malizasivongsa@malizas-MacBook-Air.local>
2021-11-10 13:48:26 -0600

    working mvp

--- static/js/templateCreator.js
+++ static/js/templateCreator.js
@@ -1,22 +1,24 @@
 'use strict';
 
-// const jsonData = require('./photocards.json')
-
-// whatever pc the user chooses will show up in the creator-container
+// whatever pc the user chooses will show up in the creator-container (left side of column)
 // need to somehow implement an if statement for whenever the user picks a pc
-// so it's added to the creator-container (left side of column)
 const addPCToContainer = (data) => {
-    $('#creator-container').append(
-        `<img src="${data}" style="height:180px;width=auto" />`)
+    $('#creator').append(
+        `<img src="${data}" style="height:140px;width=auto" />`)
+}
+
+const removePCFromContainer = (data) => {
+    $(`#creator img[src="${data}"]`).remove()
 }
 
 // takes whatever the pc the user picks from the list of pcs given (right side of column)
-$('input[type="checkbox"]').on('click', function(){
-    let selection = [];
-    $('input[type="checkbox"]:checked').each(function() {
-        selection.push($(this).val());
-    });
-    addPCToContainer($(this).val());
-    // if checked before then remove
-    // $('#creator-container').html(selection)
-});
+$('input[type="checkbox"]').on('click', function() {
+    if (this.checked) {
+        addPCToContainer($(this).val());
+    } else {
+        removePCFromContainer($(this).val());
+    };
+
+    console.log($(this).val())
+    console.log($(this))
+})
